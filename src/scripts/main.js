0c1d3fa61e70996133078f30ebf7455b846f676e
Maxwyame06 <maxwyame06@gmail.com>
2022-06-05 01:19:14 +0200

    ajout gsap sur le js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,69 @@
+import { gsap } from "gsap";
 function openNav() {
     document.getElementById("myNav").classList.toggle("menu_width");
     document
       .querySelector(".custom_menu-btn")
       .classList.toggle("menu_btn-style");
   }
+
+  let tl = gsap.timeline({
+    scrollTrigger:{
+      trigger: '.mockup-landing',
+      start: "center",
+      end:".section__footer",
+      // scrub: true,
+      markers: true,
+      toggleActions: "play none play reverse"
+
+    }
+  });
+  tl.from(".set2",{
+    y: 200,
+    oppacity: 0,
+    duration: 1
+  }, "-=1");
+
+  let tel = gsap.timeline({
+    scrollTrigger:{
+      trigger: '.box3',
+      start: "bottom",
+      end:".section__footer",
+      // scrub: true,
+      markers: true,
+      toggleActions: "play none play reverse"
+
+    }
+  });
+  tel.from(".set4",{
+    y: 200,
+    oppacity: 0,
+    duration: 1
+  }, "-=1");
+  
+
+  let ic = gsap.timeline({
+    scrollTrigger:{
+      trigger: '.infinite-slider',
+       start: "-250px",
+      // scrub: true,
+      markers: true,
+      toggleActions: "play none play reverse"
+    }
+  });
+  ic.from(".icon1", {
+    x: 1350,
+    oppacity: 0,
+    duration: 1.5
+  })
+  .from(".icon2",{
+    x: 900,
+    oppacity: 0,
+    duration: 1
+  }, "-=1")
+
+  .from(".icon3",{
+    x: 600,
+    oppacity: 0,
+    duration: 1
+  }, "-=1");
+
