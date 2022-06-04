1d2caf18f3a0adaf9ac114cef822909884b843e0
Maxwyame06 <maxwyame06@gmail.com>
2022-06-05 01:11:23 +0200

    ajout code menu js landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
-console.info('Hello world');
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document
+      .querySelector(".custom_menu-btn")
+      .classList.toggle("menu_btn-style");
+  }
